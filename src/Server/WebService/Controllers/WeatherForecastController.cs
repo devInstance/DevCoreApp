9582bd96755436519c13aecbbd62847016f2b535
yfranz <yfranz@gmail.com>
2023-04-17 20:38:52 -0700

    Minor update to the model and interfaces

--- src/Server/WebService/Controllers/WeatherForecastController.cs
+++ src/Server/WebService/Controllers/WeatherForecastController.cs
@@ -37,7 +37,6 @@ public class WeatherForecastController : BaseController
     /// <returns></returns>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public ActionResult<ModelList<WeatherForecastItem>> GetItems(int? top, int? page, int? filter, int? fields, string search = null)
     {
         return HandleWebRequest<ModelList<WeatherForecastItem>>(() =>
@@ -53,7 +52,7 @@ public class WeatherForecastController : BaseController
     [HttpGet]
     [Route("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public ActionResult<WeatherForecastItem> GetById(string id)
     {
         return HandleWebRequest((WebHandler<WeatherForecastItem>)(() =>
