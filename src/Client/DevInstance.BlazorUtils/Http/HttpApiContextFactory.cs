45891edbd674c509bc92322f0ccde9385f76fe9c
yfranz <yfranz@gmail.com>
2024-11-25 18:27:13 -0800

    Moved BlazorToolkit into separate repo

--- src/Client/DevInstance.BlazorUtils/Http/HttpApiContextFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevInstance.BlazorToolkit.Http;
-
-public class HttpApiContextFactory
-{
-    public static IApiContext<T> Create<T>(HttpClient http, string url)
-    {
-        return new HttpApiContext<T>(url, http);
-    }
-}
--- src/Client/DevInstance.BlazorUtils/Services/ServiceActionError.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DevInstance.BlazorToolkit.Services;
-
-public class ServiceActionError
-{
-    //TODO: This class will be extended with additional attributes
-    public string Message { get; set; }
-}
