87a53608e259f31ae72d0ca22541669233eb7cae
Yuriy Frankiv <yuriy@devinstance.net>
2026-02-26 15:43:25 -0800

    Added email updates migration

---
+++ src/Server/Database/Postgres/Migrations/20260226202438_EmailUpdates.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevInstance.DevCoreApp.Server.Database.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailUpdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProviderMessageId",
+                table: "EmailLogs",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProviderMessageId",
+                table: "EmailLogs");
+        }
+    }
+}
